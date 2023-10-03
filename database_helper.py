2859fda3a66c025e0a70a1cee00d24f47c75cd5d
Kyle Cook <kcook@codeevoke.com>
2023-10-03 15:16:46 -0700

    More work

---
+++ database_helper.py
@@ -0,0 +1,118 @@
+import sqlite3
+import xbmc
+
+DATABASE_NAME = 'kodi_data.db'
+
+def create_database():
+    conn = sqlite3.connect(DATABASE_NAME)
+    c = conn.cursor()
+    
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS kodi_boxes (
+            id INTEGER PRIMARY KEY,
+            actor TEXT,
+            IP TEXT,
+            encoder_url TEXT
+        )
+    ''')
+
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS addresses (
+            foreign_address TEXT PRIMARY KEY,
+            first_seen TEXT
+        )
+    ''')
+
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS links (
+            link TEXT PRIMARY KEY,
+            timestamp TEXT
+        )
+    ''')
+
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS active_streams (
+            id INTEGER PRIMARY KEY,
+            box_id INTEGER,
+            status TEXT CHECK(status IN ('Active', 'Inactive')),
+            link TEXT,
+            FOREIGN KEY (box_id) REFERENCES kodi_boxes(id)
+        )
+    ''')
+
+    conn.commit()
+    conn.close()
+
+def populate_kodi_boxes():
+    try:
+        conn = sqlite3.connect(DATABASE_NAME)
+        c = conn.cursor()
+
+        for box in KODI_BOXES:
+            c.execute(
+                'INSERT OR IGNORE INTO kodi_boxes (Actor, IP, Encoder_URL) VALUES (?, ?, ?)',
+                (box["Actor"], box["IP"], box["Encoder_URL"])
+            )
+        
+        conn.commit()
+        conn.close()
+    except sqlite3.Error as e:
+        xbmc.log(f"Error populating kodi_boxes table: {e}", level=xbmc.LOGERROR)
+
+   
+def query_database(sql, parameters=()):
+    try:
+        conn = sqlite3.connect(DATABASE_NAME)
+        c = conn.cursor()
+        c.execute(sql, parameters)
+        rows = c.fetchall()
+        conn.close()
+        return rows
+    except sqlite3.Error as e:
+        xbmc.log(f"Database query error: {e}", level=xbmc.LOGERROR)
+        return []
+
+def modify_database(sql, parameters=()):
+    try:
+        conn = sqlite3.connect(DATABASE_NAME)
+        c = conn.cursor()
+        c.execute(sql, parameters)
+        conn.commit()
+        conn.close()
+    except sqlite3.Error as e:
+        xbmc.log(f"Database modification error: {e}", level=xbmc.LOGERROR)
+
+def store_address(foreign_address, first_seen):
+    modify_database(
+        'INSERT OR IGNORE INTO addresses (foreign_address, first_seen) VALUES (?, ?)',
+        (foreign_address, first_seen)
+    )
+
+def store_link(link):
+    timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
+    modify_database(
+        'INSERT OR IGNORE INTO links (link, timestamp) VALUES (?, ?)',
+        (link, timestamp)
+    )
+
+def insert_into_active_streams(IP, status, link):
+    modify_database(
+        'INSERT INTO active_streams (IP, status, link) VALUES (?, ?, ?)',
+        (IP, status, link)
+    )
+
+def update_active_stream_status(id, status):
+    modify_database(
+        'UPDATE active_streams SET status = ? WHERE id = ?',
+        (status, id)
+    )
+
+def truncate_addresses_table():
+    try:
+        conn = sqlite3.connect(DATABASE_NAME)
+        c = conn.cursor()
+        c.execute("DELETE FROM addresses")
+        conn.commit()
+        conn.close()
+    except sqlite3.Error as e:
+        xbmc.log(f"Error truncating addresses table: {e}", level=xbmc.LOGERROR)
---
+++ kodi_rpc.py
@@ -0,0 +1,66 @@
+import json
+from urllib.request import urlopen, Request
+from urllib.error import HTTPError
+import xbmc
+
+
+def send_jsonrpc(kodi_url, payload, headers=None):
+    """
+    Send a JSON-RPC request to a Kodi instance, whether it's local or remote.
+    
+    Parameters:
+    - kodi_url (str): The URL of the Kodi instance.
+    - payload (dict): The JSON-RPC payload.
+    - headers (dict, optional): Any additional headers for the request.
+    """
+    
+    # If it's a local request
+    if kodi_url == 'local':
+        command = json.dumps(payload)
+        response = xbmc.executeJSONRPC(command)
+        return json.loads(response)
+    
+    # If it's a remote request
+    req = Request(kodi_url, json.dumps(payload).encode('utf-8'), headers or {})
+    try:
+        response = urlopen(req)
+        response_content = response.read().decode('utf-8')
+        return json.loads(response_content)
+    except HTTPError as e:
+        xbmc.log(f"Error sending JSON-RPC to remote Kodi: {e}", level=xbmc.LOGERROR)
+        return None
+
+
+def stop_kodi_playback(kodi_ip):
+    payload = {
+        "jsonrpc": "2.0",
+        "method": "Player.Stop",
+        "params": {
+            "playerid": 1
+        },
+        "id": 1
+    }
+    send_jsonrpc(payload, f"http://{kodi_ip}:8080/jsonrpc")
+
+def get_encoder_url_for_link(link):
+    rows = query_database(
+        'SELECT IP FROM active_streams WHERE link = ? AND status = "Active"',
+        (link,)
+    )
+    if rows:
+        for kodi_box in KODI_BOXES:
+            if kodi_box['IP'] == rows[0][0]:
+                return kodi_box['Encoder_URL']
+    return None
+
+def get_available_kodi_box():
+    rows = query_database(
+        'SELECT IP FROM active_streams WHERE status = "Inactive"'
+    )
+    if rows:
+        for kodi_box in KODI_BOXES:
+            if kodi_box['IP'] == rows[0][0]:
+                return kodi_box
+    return None
+
+
